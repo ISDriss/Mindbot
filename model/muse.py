42fba58b1a60b4d0ab93527597f8afbb03928e40
Ilian Sebti <ilian.sebti@gmail.com>
2024-11-29 10:55:02 +0100

    switched from threading to multiprocessing (true parallelism go brrrrr)

--- model/muse.py
+++ model/muse.py
@@ -1,22 +1,36 @@
 import muselsl as msl
-from threading import Thread
+from multiprocessing import Process
 import time
+
+def start_stream(address):
+    msl.stream(address)
+
+def start_view(version):
+    msl.view(version= version)
 
 def setup():
     address = ""
-    stream = Thread(target=msl.stream, args=(address,))
-    view = Thread(target=msl.view)
 
+    # List available Muses
     muses = msl.list_muses()
-    if(len(muses) < 1):
-        print("no devices found")
+    if len(muses) < 1:
+        print("No devices found")
         return
-    print(muses[0]["address"]) 
+
+    # Get the address of the first Muse
     address = muses[0]["address"]
+    print(f"Device address: {address}")
 
+    # Start the stream process
+    stream_process = Process(target=start_stream, args=(address,))
+    stream_process.start()
 
-    stream.run()
+    # Give the stream some time to start
     time.sleep(15)
-    view.start()
 
-setup()
+    # Start the viewer process
+    view_process = Process(target=start_view, args=(2,))
+    view_process.start()
+
+if __name__ == "__main__":
+    setup()
