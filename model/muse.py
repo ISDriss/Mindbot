aedbed5030d42faa01d784f2728c1c3a2eb65944
Ilian Sebti <ilian.sebti@gmail.com>
2024-11-29 17:52:24 +0100

    added basic recording

--- model/muse.py
+++ model/muse.py
@@ -1,12 +1,16 @@
+import os
+import time
 import muselsl as msl
 from multiprocessing import Process
-import time
 
-def start_stream(address):
+def stream(address):
     msl.stream(address)
 
-def start_view(version):
-    msl.view(version= version)
+def view(version):
+    msl.view(version=version)
+
+def record(duration, filename):
+    msl.record(duration=duration,filename=filename)
 
 def setup():
     address = ""
@@ -22,15 +26,22 @@ def setup():
     print(f"Device address: {address}")
 
     # Start the stream process
-    stream_process = Process(target=start_stream, args=(address,))
+    stream_process = Process(target=stream, args=(address,))
     stream_process.start()
 
     # Give the stream some time to start
     time.sleep(15)
 
     # Start the viewer process
-    view_process = Process(target=start_view, args=(2,))
+    view_process = Process(target=view, args=(2,))
     view_process.start()
+
+    # record file to the correct folder
+    data_folder = os.path.join(os.getcwd(), "data")
+    filename = os.path.join(data_folder, "%s_recording_%s.csv" %("EEG",time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())))
+
+    record_process = Process(target=record, args=(20,filename))
+    record_process.start()
 
 if __name__ == "__main__":
     setup()
