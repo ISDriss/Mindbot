4d8bda250414d3440c08ca971ace1608fb81478c
Ilian SEBTI <ilian.sebti@gmail.com>
2025-03-06 13:04:29 +0100

    add record_until and a record selection dialog

--- script/main.py
+++ script/main.py
@@ -1,8 +1,8 @@
 import os
 import time
 import muselsl as msl
-from multiprocessing import Process
-from muse_record import record
+from multiprocessing import Process, Event
+from muse_record import record_for, record_until
 
 def stream(address):
     msl.stream(address)
@@ -26,22 +26,44 @@ def setup():
     # Start the stream process
     stream_process = Process(target=stream, args=(address,))
     stream_process.start()
-
-    # Give the stream some time to start
-    time.sleep(15)
+    time.sleep(12) # Wait for the stream to start
 
     # Start the viewer process
     view_process = Process(target=view, args=(2,))
     view_process.start()
+    time.sleep(5) # Wait for the viewer to start
 
     # record file to the correct folder
     data_folder = os.path.join(os.getcwd(), "data")
     filename = os.path.join(data_folder, "%s_recording_%s.csv" %("EEG",time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())))
 
-    record_process = Process(target=record, args=(20,filename))
-    record_process.start()
+    # Recording selection
+    record_mode = input("Enter 1 to record until stopped, 2 to record for a set time: ")
 
-    #end it all
+    if record_mode == "1" :
+        # record until stopped
+        stop_event = Event()
+        record_process = Process(target=record_until, args=(stop_event, filename))
+        record_process.start()
+        time.sleep(5) # Wait for the recording to start
+
+        # Wait for user input to stop recording
+        user_input = input("Press Enter to stop recording...")
+        stop_event.set()
+    else :
+        # record for a set time
+        record_time = input("Enter the time in seconds to record for : ")
+        try:
+            record_time = int(record_time)
+        except ValueError:
+            print("Invalid input, recording aborted")
+            view_process.kill()
+            stream_process.kill()
+            return
+        record_process = Process(target=record_for, args=(record_time,filename))
+        record_process.start()
+
+    # end all processes
     record_process.join()
     view_process.kill()
     stream_process.kill()
