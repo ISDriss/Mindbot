be6ad373d4e8d58c38c7b9ce168367457eaa8ba3
Ilian SEBTI <ilian.sebti@gmail.com>
2025-03-11 17:18:05 +0100

    arduino change: updates on change only

--- script/arduino_controller/arduino_controller.ino
+++ script/arduino_controller/arduino_controller.ino
@@ -3,6 +3,9 @@ const int right = 3;
 const int forward = 4;
 const int backward = 5;
 
+// Variables pour stocker l'état précédent des boutons
+int last_b1 = 0, last_b2 = 0, last_b3 = 0, last_b4 = 0;
+
 void setup() {
     Serial.begin(9600);
     pinMode(left, INPUT_PULLUP);
@@ -12,19 +15,28 @@ void setup() {
 }
 
 void loop() {
+    // Lire l'état actuel des boutons
     int b1 = digitalRead(left) == LOW ? 1 : 0;
     int b2 = digitalRead(right) == LOW ? 1 : 0;
     int b3 = digitalRead(forward) == LOW ? 1 : 0;
     int b4 = digitalRead(backward) == LOW ? 1 : 0;
-    
-    Serial.print(b1);
-    Serial.print(",");
-    Serial.print(b2);
-    Serial.print(",");
-    Serial.print(b3);
-    Serial.print(",");
-    Serial.println(b4);
-    
-    delay(14); //If the delay is less than 14, the python code becomes unable to decode the data
-}
 
+    // Vérifier si l'état a changé
+    if (b1 != last_b1 || b2 != last_b2 || b3 != last_b3 || b4 != last_b4) {
+        Serial.print(b1);
+        Serial.print(",");
+        Serial.print(b2);
+        Serial.print(",");
+        Serial.print(b3);
+        Serial.print(",");
+        Serial.println(b4);
+
+        // Mettre à jour les derniers états connus
+        last_b1 = b1;
+        last_b2 = b2;
+        last_b3 = b3;
+        last_b4 = b4;
+    }
+    
+    delay(1); // Petit délai pour éviter une surcharge inutile
+}
